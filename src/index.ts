93688d74ce06244938c2d61e0ea6041d0b09a6d2
Gregory Shlyapkin <shlyapkin.g@gmail.com>
2017-05-17 22:38:26 +0700

    Small fix related to exports.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export const MPDClient = require('./lib/Client');
-export const MPDCommand = require('./lib/Command');
-export const MPDCommandList = require('./lib/CommandList');
+export const MPDClient = require('./lib/Client').MPDClient;
+export const MPDCommand = require('./lib/Command').MPDCommand;
+export const MPDCommandList = require('./lib/CommandList').MPDCommandList;
