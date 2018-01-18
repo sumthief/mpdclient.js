febb84643b84785c2960d4d3a3e76c0a317c6580
Gregory Shlyapkin <shlyapkin.g@gmail.com>
2018-01-18 23:33:41 +0700

    Ran prettify on all files, add "search" operation to ResponseParser.
    Tag

--- src/lib/Error.ts
+++ src/lib/Error.ts
@@ -5,22 +5,20 @@
  * separately from NodeJS and other errors.
  */
 export class Error {
+    /**
+     * Built markup which is ready to output for use.
+     */
+    private output: string;
 
-  /**
-   * Built markup which is ready to output for use.
-   */
-  private output: string;
-
-  /**
-   * Constructs an error.
-   *
-   * @param {[string|number]} errorInfo - Array of strings and numbers describing returned error.
-   *
-   * @see ResponseParser.RESPONSE_PARSER_ERROR_PATTERN for details.
-   */
-  constructor(errorInfo: Array<any>) {
-    const [code, cmdPos, cmd, text] = errorInfo;
-    this.output = `MPD returned an error: ${text} (command: ${cmd}, code: ${code}, position: ${cmdPos})`;
-  }
-
+    /**
+     * Constructs an error.
+     *
+     * @param {[string|number]} errorInfo - Array of strings and numbers describing returned error.
+     *
+     * @see ResponseParser.RESPONSE_PARSER_ERROR_PATTERN for details.
+     */
+    constructor(errorInfo: Array<any>) {
+        const [code, cmdPos, cmd, text] = errorInfo;
+        this.output = `MPD returned an error: ${text} (command: ${cmd}, code: ${code}, position: ${cmdPos})`;
+    }
 }
--- src/lib/IExecutable.ts
+++ src/lib/IExecutable.ts
@@ -1,15 +1,13 @@
 export interface IExecutable {
+    /**
+     * Query string which will be written to socket.
+     */
+    query: string;
 
-  /**
-   * Query string which will be written to socket.
-   */
-  query: string;
-
-  /**
-   * Build query string.
-   *
-   * @return string - Built query string.
-   */
-  buildQuery(): string;
-
+    /**
+     * Build query string.
+     *
+     * @return string - Built query string.
+     */
+    buildQuery(): string;
 }
